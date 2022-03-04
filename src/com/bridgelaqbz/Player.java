a798370b7acae77ed501dff3ec43710128b94ee0
jayant <jayantparmar420@gmail.com>
2022-03-04 16:17:23 +0530

    initial commit

---
+++ src/com/bridgelaqbz/Player.java
@@ -0,0 +1,11 @@
+package com.bridgelaqbz;
+
+public class Player {
+    int position;
+
+    public Player( int position )
+    {
+        this.position = position;
+    }
+}
+
---
+++ src/com/bridgelaqbz/Snake.java
@@ -0,0 +1,13 @@
+package com.bridgelaqbz;
+
+public class Snake {
+
+        int initialPosition;
+        int finalPosition;
+
+        Snake(int initialPosition, int finalPosition)
+        {
+            this.initialPosition = initialPosition;
+            this.finalPosition = finalPosition;
+        }
+    }
